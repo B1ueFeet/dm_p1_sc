f91eeb5430665c48607caabbdd128766a1d7984a
BlueFeet <b1uefeet.dev@gmail.com>
2023-11-28 19:48:51 -0500

    extend database error

--- app/src/main/java/com/castillo/test/data/entities/repository/DataBaseRepo.kt
+++ app/src/main/java/com/castillo/test/data/entities/repository/DataBaseRepo.kt
@@ -1,6 +1,7 @@
 package com.castillo.test.data.entities.repository
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.castillo.test.data.entities.dao.UsersDAO
 import com.castillo.test.data.entities.entities.User
 
@@ -8,6 +9,6 @@ import com.castillo.test.data.entities.entities.User
     entities = [User::class],
     version = 1
 )
-abstract class DataBaseRepo {
+abstract class DataBaseRepo : RoomDatabase(){
     abstract fun getUserDAO() : UsersDAO
 }
